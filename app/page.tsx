5fb81a8d52a1263a5ed67734b986355515db289f
business-account-comsq <mizutani_haruto@comsq.com>
2026-04-06 17:21:56 +0900

    initialized turso with dr

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,20 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
 export default function Home() {
   return (
-    <div>Hello World</div>    
+    <main className="max-w-2xl mx-auto p-10">
+      <div className="flex flex-col justify-center gap-3">
+        <Input placeholder="This is my input component" />
+        <Button>Submit</Button>
+      </div>
+      <div className="p-5">
+        <ul>
+          <li>This is first message</li>
+          <li>This is second message</li>
+          <li>This is third message</li>
+        </ul>
+      </div>
+    </main>
   );
 }
