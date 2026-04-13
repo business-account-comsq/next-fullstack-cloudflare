0e93fab1e2ec46b8f343b062ca19660b0f0cbef0
business-account-comsq <mizutani_haruto@comsq.com>
2026-04-13 18:06:32 +0900

    debug index.ts

--- app/db/index.ts
+++ app/db/index.ts
@@ -2,10 +2,15 @@ import { drizzle } from "drizzle-orm/libsql/web";
 import { createClient } from "@libsql/client/web";
 
 export function getDb() {
+  console.log("① getDb start");
+  console.log("② env check", {
+    hasUrl: !!process.env.DATABASE_URL,
+    hasToken: !!process.env.DATABASE_TOKEN,
+  });
   const client = createClient({
     url: process.env.DATABASE_URL!,
     authToken: process.env.DATABASE_TOKEN!,
   });
-
+  console.log("③ client created");
   return drizzle(client);
 }
