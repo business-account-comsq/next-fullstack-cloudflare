0f2c1e57a9029c1de5ae57206a4a1753631b9c51
business-account-comsq <mizutani_haruto@comsq.com>
2026-04-13 16:50:18 +0900

    fix index.tsVI

--- app/db/index.ts
+++ app/db/index.ts
@@ -1,5 +1,5 @@
 import { drizzle } from "drizzle-orm/libsql";
-import { createClient } from "@libsql/client/http";
+import { createClient } from "@libsql/client/web";
 
 export function getDb() {
   const client = createClient({
